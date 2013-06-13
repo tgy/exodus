4eb8e67c7712506dbfd0767ed2e9a289718ef21b
Grimpow <gpw@gpw.fr>
2013-06-13 14:57:44 +0200

    .
    Signed-off-by: Grimpow <gpw@gpw.fr>

---
+++ Exodus/Exodus/PlayGame/Tasks/ChangeResources/HEIToS.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Exodus.PlayGame.Items.Units;
+
+namespace Exodus.PlayGame.Tasks.ChangeResources
+{
+    [Serializable]
+    class HEIToS : ChangeResource
+    {
+        public HEIToS(Item parent)
+            : base(parent, 20000, new Resource(0, 100, 0, 10, 50), new Resource(50, 0, 0, 0, 0))
+        {
+        }
+    }
+}

433ee9e2971547fd545fbecfffaef3da32592e53
Grimpow <gpw@gpw.fr>
2013-06-14 12:32:32 +0200

    jk
    Signed-off-by: Grimpow <gpw@gpw.fr>

--- Exodus/Exodus/GUI/Item.cs
+++ Exodus/Exodus/GUI/Item.cs
@@ -27,17 +27,16 @@ namespace Exodus.GUI
 
         public virtual void Update(GameTime gameTime)
         {
-            foreach (var c in Components)
-            {
-                if (c.ClickFocus)
+            for (int i = 0; i < Components.Count; i++)
+                if (Components[i].ClickFocus)
                 {
                     if (Inputs.LeftClick())
-                        c.Focused = c.Area.Contains(Inputs.MouseState.X, Inputs.MouseState.Y);
+                        Components[i].Focused = Components[i].Area.Contains(Inputs.MouseState.X, Inputs.MouseState.Y);
                 }
                 else
-                    c.Focused = c.Area.Contains(Inputs.MouseState.X, Inputs.MouseState.Y);
+                    Components[i].Focused = Components[i].Area.Contains(Inputs.MouseState.X, Inputs.MouseState.Y);
 
-                c.Update(gameTime);
+                Components[i].Update(gameTime);
             }
         }
 

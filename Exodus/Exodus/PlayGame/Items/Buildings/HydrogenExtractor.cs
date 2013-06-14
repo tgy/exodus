f424cbf8421bb7b03823d5a87cbac7ac366a8fb2
Zehir <thibaudmichaud.1@gmail.com>
2013-06-14 19:16:55 +0200

    ...

---
+++ Exodus/Exodus/PlayGame/Items/Buildings/HydrogenExtractor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Exodus.PlayGame.Items.Buildings
+{
+    class HydrogenExtractor : Building
+    {
+        public HydrogenExtractor(int IdPlayer)
+        {
+            Name = "Hydrogen extractor";
+            maxLife = 666;
+            maxShield = 666;
+            Width = 2;
+            this.IdPlayer = IdPlayer;
+            base.Initialize(Int32.MaxValue, 1, 20, 6);
+            this.resourcesGeneration.Electricity = 5;
+            this.resourcesGeneration = new Resource(0, 0, 0, 5, 0);
+        }
+    }
+}
---
+++ Exodus/Exodus/PlayGame/Items/Obstacles/Iron.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Exodus.PlayGame.Items.Obstacles
+{
+    [Serializable]
+    class Iron : Obstacle
+    {
+        public int Value = 5000;
+        public Iron()
+        {
+            Name = "Iron";
+            maxLife = 666;
+            maxShield = 666;
+            Width = 2;
+            base.Initialize(Int32.MaxValue, 1, 0, 0);
+        }
+
+        protected override void UpdateAnim()
+        {
+            anim = (int)(Value > 0 ? Animation.Anim : Animation.Stand);
+        }
+    }
+}

60a4452b997ad9d66fdbaf01d28a35f457ccc697
Zehir <thibaudmichaud.1@gmail.com>
2013-06-15 23:19:31 +0200

    AI

--- Exodus/Exodus/PlayGame/AI.cs
+++ Exodus/Exodus/PlayGame/AI.cs
@@ -22,7 +22,7 @@ namespace Exodus.PlayGame
         {
             if (task == 0 && time >= new TimeSpan(0, 0, 0, 2))
             {
-                baseAI = (Items.Buildings.Habitation)First<Items.Buildings.Habitation>(true);
+                baseAI = (Items.Buildings.Habitation)Map.ListItems.FirstOrDefault(x => x is Items.Buildings.Habitation && x.IdPlayer == id);
                 if (baseAI != null)
                 {
                     Items.Units.Worker w;
@@ -65,35 +65,39 @@ namespace Exodus.PlayGame
                     }
                     task++;
                 }
-                else if ((task - 4) % 3 == 0 && time >= new TimeSpan(0, 0, 0, 15) && (time.Seconds - 15) % 15 == 0)
+                else if ((task - 4) % 4 == 0 && time >= new TimeSpan(0, 0, 0, 15) && (time.Seconds - 15) % 20 == 0)
                 {
-                    for (int i = 0; i < rand.Next(10); i++)
+                    for (int i = 0; i < rand.Next(5); i++)
                     {
                         ProduceRandomUnit();
                     }
                     task++;
                 }
-                else if ((task - 5) % 3 == 0 && time >= new TimeSpan(0, 0, 0, 20) && (time.Seconds - 20) % 15 == 0)
+                else if ((task - 5) % 4 == 0 && time >= new TimeSpan(0, 0, 0, 20) && (time.Seconds - 20) % 20 == 0)
                 {
-                    GetNUnitAndAttackRandomItem(rand.Next(10));
+                    GetNUnitAndAttackRandomItem(rand.Next(5));
                     task++;
                 }
-                else if ((task - 6) % 3 == 0 && time >= new TimeSpan(0, 0, 0, 25) && (time.Seconds - 25) % 15 == 0)
+                else if ((task - 6) % 4 == 0 && time >= new TimeSpan(0, 0, 0, 25) && (time.Seconds - 25) % 20 == 0)
                 {
                     GetUselessUnitToBase();
                     task++;
                 }
+                else if ((task - 7) % 4 == 0 && time >= new TimeSpan(0, 0, 0, 35) && (time.Seconds - 35) % 20 == 0)
+                {
+                    if (rand.Next(9) == 0)
+                    {
+                        Items.Units.Worker worker = (Items.Units.Worker)Map.ListItems.FirstOrDefault(x => x is Items.Units.Worker && x.IdPlayer == id && x.TasksList.Count == 0);
+                        if (worker != null)
+                        {
+                            Point p = new Point(rand.Next(baseAI.pos.Value.X - 20, baseAI.pos.Value.X + 20), rand.Next(baseAI.pos.Value.Y - 20, baseAI.pos.Value.Y + 20));
+                            if (Map.MapCells[p.X, p.Y].ListItems.Count == 0)
+                                worker.AddTask(new Tasks.ProductItem(worker, Data.GameInfos.timeCreatingItem[typeof(Items.Buildings.Habitation)], new Items.Buildings.Habitation(id), p, true, true, true), false, false);
+                        }
+                        task++;
+                    }
+                }
             }
-        }
-
-        private static Item First<T>(bool activeItem)
-        {
-            return (activeItem ? Map.ListItems : Map.ListPassiveItems).FirstOrDefault(x => x is T && x.IdPlayer == id);
-        }
-
-        private static Item First<T>(bool activeItem, List<Item> different)
-        {
-            return (activeItem ? Map.ListItems : Map.ListPassiveItems).FirstOrDefault(x => x is T && x.IdPlayer == id && !different.Contains(x));
         }
 
         private static Item Closest<T>(bool activeItem)
@@ -119,18 +123,18 @@ namespace Exodus.PlayGame
             {
                 if (i is Items.Buildings.Habitation && i.IdPlayer == id)
                 {
-                    switch (rand.Next(4))
+                    switch (rand.Next(7))
                     {
-                        case 0:
+                        case 0: case 1:
                             i.AddTask(new Tasks.ProductItem(i, Data.GameInfos.timeCreatingItem[typeof(Items.Units.Gunner)], new Items.Units.Gunner(id), i.pos.Value, true, true, false), false, false);
                             break;
-                        case 1:
+                        case 2: case 3:
                             i.AddTask(new Tasks.ProductItem(i, Data.GameInfos.timeCreatingItem[typeof(Items.Units.Laserman)], new Items.Units.Laserman(id), i.pos.Value, true, true, false), false, false);
                             break;
-                        case 2:
+                        case 4: case 5:
                             i.AddTask(new Tasks.ProductItem(i, Data.GameInfos.timeCreatingItem[typeof(Items.Units.Spider)], new Items.Units.Spider(id), i.pos.Value, true, true, false), false, false);
                             break;
-                        case 3:
+                        case 6:
                             i.AddTask(new Tasks.ProductItem(i, Data.GameInfos.timeCreatingItem[typeof(Items.Units.Worker)], new Items.Units.Worker(id), i.pos.Value, true, true, false), false, false);
                             break;
                     }
